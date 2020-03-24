1b790c4ebf2f45de9f8b37fff4340ac333666680
scrabill <shannon.crabill@gmail.com>
2020-03-24 13:00:52 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+
+  // state = {
+  //   reviews: []
+  // }
+
+  componentDidMount() {
+    fetch(URL)
+    .then((response) => {
+      return response.json();
+    })
+    // .then((data) => {
+    //   console.log(data);
+    // });
+    .then(data => this.setState({
+      reviews: data.results
+    }))
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,68 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     reviews: [],
+  //     searchTerm: ''
+  //   }
+  // }
+
+  state = {
+    reviews: [],
+    searchTerm: ''
+  }
+
+  // https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ&query=crime
+
+  // componentDidMount(searchTerm) {
+  //   fetch(URL + `?query=${searchTerm}`)
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   // .then((data) => {
+  //   //   console.log(data);
+  //   // });
+  //   .then(data => this.setState({
+  //     reviews: data.reviews
+  //   }))
+  // }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    // console.log(this.state.searchTerm) // crime
+    // this.props.fetchReviews(this.state.searchTerm)
+    // let searchTerm = event.target.value
+
+    // console.log(`Searching for: ${this.state.searchTerm}`)
+
+    fetch(URL + `&query=${this.state.searchTerm}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then(data => this.setState({
+      reviews: data.results // results or reviews
+    }))
+  }
+
+  handleSearch = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      searchTerm: event.target.value
+    }, console.log("The query is currently:", this.state.searchTerm))
+    // console.log("The query is currently:", this.state.query)
+  }
+
+  render() {
+    return (
+      <form className="SearchableMovieReviewsContainer" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Enter a search query" value={this.state.query} onChange={this.handleSearch} />
+
+        <MovieReviews reviews={this.state.reviews} />
+      </form>
+    )
+  }
+}
