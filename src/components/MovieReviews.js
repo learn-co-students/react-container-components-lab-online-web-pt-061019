eaae04b2b613e0c9118ebcf218b9ddaeb409803b
Brittany Javalera <brittjavalera@gmail.com>
2020-03-24 18:12:41 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,18 +1,16 @@
 // Code MovieReviews Here
 import React from 'react';
 
-const MovieReviews = (props) => {
+const MovieReviews = props=> {
     return (
     <div className="review-list">
-     {props.reviews.map(review => {
-    return(
+     {props.reviews.map(review => (
      <div className="review" key={review.display_title}>
      <h2>{review.headline}</h2>
      <h4>{review.byline}</h4>
      <p>{review.summary_short}</p>
      </div>
      )
-     }
      )}
     </div>
 )}
