b5bbf1d854669115da3d72cba9a26645d1fc441e
P0rtk3y <llama.p0rtk3y@gmail.com>
2020-03-27 00:10:01 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,25 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviews extends Component {
+    state = {
+        reviews: []
+    }
+
+    componentDidMount(){
+        fetch(URL)
+            .then(resp => resp.json())
+            .then(movieData => this.setState({reviews: movieData.results}))
+            .catch(error => console.log(error))
+    }
+
+    render(){
+        return(
+            <div className='latest-movie-reviews'>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+
+export default LatestMovieReviews;
